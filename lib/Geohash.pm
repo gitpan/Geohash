fd2c9b1136c90bfe23340eaaf094888e95ec2c22
Kazuhiro Osawa <yappo+cpan@shibuya.pl>
2012-10-05 07:04:58 +0000

    Import of YAPPO/Geohash-0.03 from CPAN.
    gitpan-cpan-distribution: Geohash
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         YAPPO/Geohash-0.03.tar.gz
    gitpan-cpan-author:       YAPPO
    gitpan-cpan-maturity:     released

--- lib/Geohash.pm
+++ lib/Geohash.pm
@@ -1,7 +1,7 @@
 package Geohash;
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Exporter 'import';
 our @EXPORT_OK   = qw( ADJ_TOP ADJ_RIGHT ADJ_LEFT ADJ_BOTTOM );
